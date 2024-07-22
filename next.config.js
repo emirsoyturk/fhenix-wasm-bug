e5b87e9a34567804b9f35910b1b188799de1f95f
Cashmaney <itzik@scrtlabs.com>
2024-07-22 14:28:26 +0000

    fix next build & a bit of the code

--- next.config.js
+++ next.config.js
@@ -1,18 +1,35 @@
-// @ts-check
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    webpack: config => {
-      config.resolve.fallback = { fs: false, net: false, tls: false };
-      config.externals.push("pino-pretty", "lokijs", "encoding");
-      config.experiments = { asyncWebAssembly: true, layers: true };
-      config.module.rules.push({
-        test: /\.wasm$/,
-        type: "webassembly/async",
-      });
-      return config;
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+
+module.exports = {
+  webpack: (config, { isServer }) => {
+
+    patchWasmModuleImport(config, isServer);
+
+    if (!isServer) {
+      config.output.environment = { ...config.output.environment, asyncFunction: true };
+    }
+    return config
+  }
+}
+
+function patchWasmModuleImport(config, isServer) {
+  config.experiments = Object.assign(config.experiments || {}, {
+    asyncWebAssembly: true,
+    layers: true,
+    topLevelAwait: true
+  });
+
+  config.optimization.moduleIds = 'named';
+
+  config.module.rules.push({
+    test: /\.wasm$/,
+    type: 'asset/resource',
+  });
+
+  // TODO: improve this function -> track https://github.com/vercel/next.js/issues/25852
+  if (isServer) {
+    config.output.webassemblyModuleFilename = './../static/wasm/tfhe_bg.wasm';
+  } else {
+    config.output.webassemblyModuleFilename = 'static/wasm/tfhe_bg.wasm';
+  }
+}
